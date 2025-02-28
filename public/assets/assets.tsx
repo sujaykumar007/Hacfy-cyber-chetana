39ba7ce0504567e82a8ea72bdf6ebf6d8303f60d
manojseetaram <manojseetaram.artytech@gmail.com>
2025-02-28 15:55:56 +0530

    create navabra and footer

---
+++ public/assets/assets.tsx
@@ -0,0 +1,8 @@
+import logo from "./cyberprobex.png"
+import menublack from "./menu.png"
+import menuwhite from "./menu-white.png"
+export const assets ={
+    logo,
+    menublack,
+    menuwhite
+}
